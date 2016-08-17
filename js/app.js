add64337fe293e374009a7c530f3cb947a78346d
kathyzhang <kx.zhang@mail.utoronto.ca>
2016-08-17 01:38:03 -0400

    implement weather, current map area search, search results detail rendering

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 var _allSearchTypes = [];
 var _allResultLists = ko.observableArray([]);
-var _address;
+var _addressInput = document.getElementById('pac-input');
 var _geocoder;
 var infowindow;
 var _markers = [];
+var map;
+var pos;
 
-function geocodeAddress(placeType, resultsMap) {
-
-  // var address = document.getElementById('address').value;
-  _geocoder.geocode({'address': _address}, function(results, status) {
+function geocodeAddress(placeType) {
+  _geocoder.geocode({'address': _addressInput}, function(results, status) {
     if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
+      map.setCenter(results[0].geometry.location);
       infowindow = new google.maps.InfoWindow();
       var service = new google.maps.places.PlacesService(map);
+      pos = results[0].geometry.location.toJSON();
+
       service.nearbySearch({
         location: results[0].geometry.location,
         radius: 500,
@@ -27,9 +29,20 @@ function geocodeAddress(placeType, resultsMap) {
 }
 
 
+function searchCurrentMapArea(placeType) {
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: map.getCenter(),
+    radius: 500,
+    type: placeType,
+  }, function(results, status) {
+    processResults(results, status, placeType);
+  });
+}
+
+
 function initMap() {
   var pyrmont = {lat: -33.867, lng: 151.195};
-
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15
   });
@@ -37,7 +50,7 @@ function initMap() {
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
@@ -54,20 +67,57 @@ function initMap() {
   _geocoder = new google.maps.Geocoder();
 }
 
+
 function processResults(results, status, placeType) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
+    // TODO: Is ranking this way necessary?
     var rankedResult = results.sort(function(a, b){return b.rating-a.rating});
-    renderResults(results, placeType);
-    console.log("got results")
+    renderResults(rankedResult, placeType);
+    storePlaceDetails(rankedResult, placeType);
+  }
+}
 
-    var newResultsList = new resultsList();
-    newResultsList.typeName = placeType;
-    for (var i = 0; i < rankedResult.length; i++) {
-      newResultsList.resultItemList.push({name: rankedResult[i].name, rating: rankedResult[i].rating});
-    }
-    _allResultLists.push(newResultsList);
-    console.log(newResultsList.typeName);
 
+function storePlaceDetails(results, placeType) {
+  var newResultsList = new resultsList();
+  newResultsList.typeName = placeType;
+
+  for (var i = 0; i < results.length; i++) {
+    var place = results[i];
+    var item = {
+      name: place.name,
+      rating: place.rating,
+      address: place.vicinity,
+      icon: place.icon,
+      id: place.place_id,
+      hours: ''
+    };
+    newResultsList.resultItemList.push(item);
+  }
+  //console.log("hours:" + item.hours);
+  var index = _allResultLists.push(newResultsList) - 1;
+  console.log(newResultsList.typeName);
+
+  //getDetialedHours(index);
+}
+
+
+function getDetialedHours(index) {
+  var results = _allResultLists()[index].resultItemList();
+  var service = new google.maps.places.PlacesService(map);
+  for (var i = 0; i < results.length; i++) {
+    var place = results[i];
+    var request = { reference: place.id };
+    // Because many places don't have Google Map getDetials service.
+    // Choose to use places' common information except hours.
+    service.getDetails(request, function(placeDetails, status) {
+      console.log(status);
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          _allResultLists()[index].resultItemList[i].hours = placeDetails.opening_hours.weekday_text;
+        }
+      }
+    });
   }
 }
 
@@ -86,23 +136,20 @@ function renderResults(results, placeType) {
 function createMarker(place, placeType, timeout, ranking) {
   window.setTimeout(function() {
     var placeLoc = place.geometry.location;
-    // var image = {
-    //   url: 'img/'+ placeType + '.png',
-    //   // This marker is 20 pixels wide by 32 pixels high.
-    //   size: new google.maps.Size(20, 32),
-    //   // The origin for this image is (0, 0).
-    //   origin: new google.maps.Point(0, 0),
-    //   // The anchor for this image is the base of the flagpole at (0, 32).
-    //   anchor: new google.maps.Point(0, 32)
-    // };
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(70, 70),
+      scaledSize: new google.maps.Size(20, 20),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 0)
+    };
     var url = 'img/'+ placeType + '.png';
 
     var marker = new google.maps.Marker({
       map: map,
-      icon: url,
+      icon: image,
       // label: (place.rating > 0 ? String(place.rating) : "?"),
       // label: (ranking < 10 ? ranking : ""),
-      label: placeType,
       position: place.geometry.location,
       animation: google.maps.Animation.DROP
     });
@@ -122,25 +169,46 @@ var ViewModel = function() {
   self.inputAddress = ko.observable("Toronto, ON, Canada");
   self.searchType = ko.observableArray([]);
   self.allResultLists = _allResultLists();
+  self.searchTemperature = ko.observable();
+  self.description = ko.observable();
+  self.weatherIcon = ko.observable();
 
   self.updateAddressAndType = function() {
     console.log(self.searchType());
     removeAllMarkers();
+    for (var i=0 ; i<_allResultLists().length ; i++) {
+      _allResultLists()[i] = null;
+    }
     _allResultLists().length = 0;
-    _address = self.inputAddress();
+    _addressInput = self.inputAddress();
+    _allSearchTypes = self.searchType();
+    var forecastUrl = "https://api.forecast.io/forecast/6bff14d01f94dbf252e20a2715e03f52/"+pos.lat+ "," + pos.lng;
+
+    $.getJSON(forecastUrl, function(data) {
+      self.searchTemperature(data.currently.temperature);
+      self.description(data.currently.icon);
+      var skycons = new Skycons({"color": "#3385ff"});
+      skycons.add("weather-icon", data.currently.icon);
+      skycons.play();
+    }).error(function(e) {
+      console.log("weather error!!");
+      console.log(e);
+    });
 
     for (var i=0; i < self.searchType().length; i++) {
-      geocodeAddress(self.searchType()[i], map);
+      geocodeAddress(self.searchType()[i]);
     }
   }
 
-  self.toggleSearchType = function(data, event) {
-    var type = event.target.value;
-    var i = self.searchType().indexOf(type);
-    if (i == -1) {
-      self.searchType().push(type);
-    } else {
-      self.searchType().splice(i, 1);
+  self.searchMap = function() {
+    removeAllMarkers();
+    for (var i=0 ; i<_allResultLists().length ; i++) {
+      _allResultLists()[i] = null;
+    }
+    _allResultLists().length = 0;
+
+    for (var i=0; i < self.searchType().length; i++) {
+      searchCurrentMapArea(self.searchType()[i]);
     }
   }
 };
@@ -152,13 +220,17 @@ function removeAllMarkers() {
 
 function setMapOnAllMarkers(map) {
   for (var i = 0; i < _markers.length; i++) {
-       _markers[i].setMap(map);
+    _markers[i].setMap(map);
   }
 }
 
-var mapItem = function(data) {
+var placeItem = function(data) {
   this.name = ko.observable(data.name);
   this.rating = ko.observable(data.rating);
+  this.address = ko.observable(data.address);
+  this.hours = ko.observable(data.hours);
+  this.icon = ko.observable(data.icon);
+  this.id = "";
 };
 
 var resultsList = function(data) {
